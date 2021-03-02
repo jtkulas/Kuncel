76575e27935bba7865ef2ace24d5897f20aed408
Renata Garcia Prieto Palacios Roji <garciaprier1@admsu.montclair.edu>
2021-03-02 11:24:05 -0500

    outliers code

--- latencies.R
+++ latencies.R
@@ -6,3 +6,15 @@ latencies <- read.csv("Reduced_scale.csv")
 
 latencies$Q1643_First.Click <- as.numeric(as.character(latencies$Q1643_First.Click))
 hist(latencies$Q1643_First.Click)
+
+library(data.table)
+## creating function for outliers 
+outlierReplace = function(dataframe, cols, rows, newValue = NA) {
+  if (any(rows)) {
+    set(dataframe, rows, cols, newValue)
+  }
+}
+
+# same variable without outliers
+outlierReplace(latencies, "Q1643_First.Click", which(latencies$Q1643_First.Click > 600), NA )
+hist(latencies$Q1643_First.Click)
