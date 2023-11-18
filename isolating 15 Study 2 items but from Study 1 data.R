a0fa91cd025088a2f30cbdc0a2b6d3e8a3d0d4ec
John-o Kulas <kulasj@montclair.edu>
2023-11-18 10:29:47 -0600

    stalled on isolating 15 study 2 items from study 1 data source (still need to do prior to next meeting)

---
+++ isolating 15 Study 2 items but from Study 1 data.R
@@ -0,0 +1,4 @@
+## Isolating 15 Study 2 items (from Study 1 data) so Emily can run reliabilities - 11/18/23
+## each item is represented by 5 consecutive columns (15 items x 5 "response" category responses)
+
+## data a little more complex than anticipated
